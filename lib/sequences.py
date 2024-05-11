03ec8038d90da9dbbb5a51e4c03ee9ca36c8c3c6
Amy-038 <amy.sirma@student.moringaschool.com>
2024-05-11 13:25:59 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    list = []
+
+    for n in range(length):
+        if n == 0:
+            list.append(0)
+        elif n == 1:
+            list.append(1)
+        else:
+            f = list[-1] + list[-2]
+            list.append(f)
+    
+    print(list)
+
+print_fibonacci(9)
